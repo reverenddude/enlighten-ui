9bfddc08362c86cb2a7fa502c8e13340eaed2633
eric <e.grieser1@hotmail.com>
2026-04-09 12:19:32 -0500

    started working through new evidence menu

--- src/case.rs
+++ src/case.rs
@@ -1,11 +1,21 @@
+use std::path::PathBuf;
+
 use iced::Alignment::Center;
 use iced::Element;
-use iced::widget::{button, column, row, text};
+use iced::widget::{button, column, container, row, text, text_input};
+
+use enlighten::{EvidenceStore, ProcessingProfile};
 
 #[derive(Debug)]
 pub struct CasePage {
     case: enlighten::Case,
-    processing_state: ProcessingState,
+    processing_state: CasePageState,
+    new_evidence_state: NewEvidenceState,
+}
+
+#[derive(Debug)]
+struct NewEvidenceState {
+    source_path: String,
 }
 
 #[derive(Debug, Copy, Clone)]
@@ -13,6 +23,7 @@ pub enum Message {
     CloseCase,
     CancelProcessing,
     AddEvidenceStore,
+    CreateProcessingProfile,
     Process,
 }
 
@@ -21,7 +32,8 @@ pub enum NavigationMessage {
 }
 
 #[derive(Debug, Copy, Clone)]
-enum ProcessingState {
+enum CasePageState {
+    CreatingNewEvidence,
     Processing,
     NotProcessing,
 }
@@ -30,7 +42,10 @@ impl CasePage {
     pub fn new(case: enlighten::Case) -> Self {
         Self {
             case,
-            processing_state: ProcessingState::NotProcessing,
+            processing_state: CasePageState::NotProcessing,
+            new_evidence_state: NewEvidenceState {
+                source_path: String::new(),
+            },
         }
     }
 
@@ -40,8 +55,9 @@ impl CasePage {
 
     pub fn view(&self) -> Element<'_, Message> {
         match self.processing_state {
-            ProcessingState::Processing => self.processing_widget(),
-            ProcessingState::NotProcessing => self.not_processing_widget(),
+            CasePageState::Processing => self.processing_widget(),
+            CasePageState::NotProcessing => self.not_processing_widget(),
+            CasePageState::CreatingNewEvidence => self.add_new_evidence_widget(),
         }
     }
 
@@ -49,15 +65,31 @@ impl CasePage {
         match message {
             Message::AddEvidenceStore => None,
             Message::Process => {
-                self.processing_state = ProcessingState::Processing;
+                self.processing_state = CasePageState::Processing;
                 None
             }
             Message::CancelProcessing => {
-                self.processing_state = ProcessingState::NotProcessing;
+                self.processing_state = CasePageState::NotProcessing;
                 None
             }
             Message::CloseCase => Some(NavigationMessage::CloseCase),
+            Message::CreateProcessingProfile => todo!(),
         }
+    }
+
+    fn add_new_evidence_widget(&self) -> Element<'_, Message> {
+        column![
+            container(row![
+                text("Evidence Store Name"),
+                text_input("Source Path..", &self.new_evidence_state.source_path)
+            ])
+            .style(container::rounded_box)
+        ]
+        .into()
+    }
+
+    fn new_processing_profile_widget(&self) -> Element<'_, Message> {
+        todo!()
     }
 
     fn processing_widget(&self) -> Element<'_, Message> {
